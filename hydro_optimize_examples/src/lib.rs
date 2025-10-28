24fca0c99b988756732f87cdaa0f3d05d18a1edc
David Chu <davidchuyaya@gmail.com>
2025-10-28 21:27:37 +0000

    Remove toy examples

--- hydro_optimize_examples/src/lib.rs
+++ hydro_optimize_examples/src/lib.rs
@@ -1,12 +1,5 @@
 stageleft::stageleft_no_entry_crate!();
 
-pub mod simple_graphs;
-pub mod simple_graphs_bench;
-pub mod simple_kv_bench;
-pub mod lock_server;
-pub mod lobsters;
-pub mod web_submit;
-
 #[cfg(test)]
 mod test_init {
     #[ctor::ctor]
