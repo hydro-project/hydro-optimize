c2e2cbf98558a580dc2f940775f8488a900d1320
David Chu <chudc@amazon.com>
2026-03-12 16:58:46 +0000

    Launch clients on different threads, pin to cores

--- hydro_optimize/src/deploy.rs
+++ hydro_optimize/src/deploy.rs
@@ -37,6 +37,12 @@ pub struct ReusableHosts {
 // Note: Aws AMIs vary by region. If you are changing the region, please also change the AMI.
 const AWS_REGION: &str = "us-west-2";
 const AWS_INSTANCE_AMI: &str = "ami-055a9df0c8c9f681c"; // Amazon Linux 2
+const AWS_INSTANCE_TYPE: &str = "m5.2xlarge"; // 8 vCPU, 32 GB RAM
+const AWS_NUM_CORES: usize = 8; // Used for pinning
+const GCP_REGION: &str = "us-central1-c";
+const GCP_IMAGE: &str = "debian-cloud/debian-12";
+const GCP_MACHINE_TYPE: &str = "n2-standard-4"; // 4 vCPU, 16 GB RAM
+const GCP_NUM_CORES: usize = 4; // Used for pinning
 
 impl ReusableHosts {
     pub fn new(host_type: &HostType) -> Self {
@@ -63,6 +69,14 @@ impl ReusableHosts {
     pub fn insert_env(&mut self, key: String, value: String) {
         self.env.insert(key, value);
     }
+   
+    pub fn num_cores(&self) -> usize {
+        match &self.host_type {
+            InitializedHostType::Gcp { .. } => GCP_NUM_CORES,
+            InitializedHostType::Aws { .. } => AWS_NUM_CORES,
+            InitializedHostType::Localhost => 1, // Can't pin to cores locally anyway
+        }
+    }
 
     // NOTE: Creating hosts with the same display_name in the same deployment will result in undefined behavior.
     fn lazy_create_host(
@@ -76,9 +90,9 @@ impl ReusableHosts {
                 InitializedHostType::Gcp { project, network } => deployment
                     .GcpComputeEngineHost()
                     .project(project.clone())
-                    .machine_type("n2-standard-4")
-                    .image("debian-cloud/debian-12")
-                    .region("us-central1-c")
+                    .machine_type(GCP_MACHINE_TYPE)
+                    .image(GCP_IMAGE)
+                    .region(GCP_REGION)
                     .network(network.clone())
                     .display_name(display_name)
                     // Better performance than MUSL, perf reporting fewer unidentified stacks, but requires launching from Linux
@@ -87,7 +101,7 @@ impl ReusableHosts {
                 InitializedHostType::Aws { network } => deployment
                     .AwsEc2Host()
                     .region(AWS_REGION)
-                    .instance_type("m5.2xlarge")
+                    .instance_type(AWS_INSTANCE_TYPE)
                     .ami(AWS_INSTANCE_AMI)
                     .network(network.clone())
                     // Better performance than MUSL, perf reporting fewer unidentified stacks, but requires launching from Linux
@@ -120,8 +134,10 @@ impl ReusableHosts {
         &mut self,
         deployment: &mut Deployment,
         display_name: String,
+        pin_to_core: usize,
     ) -> TrybuildHost {
         let host = TrybuildHost::new(self.lazy_create_host(deployment, display_name.clone()))
+            .pin_to_core(pin_to_core)
             .rustflags(self.get_rust_flags());
         self.host_with_env(host)
     }
@@ -130,6 +146,7 @@ impl ReusableHosts {
         &mut self,
         deployment: &mut Deployment,
         display_name: String,
+        pin_to_core: usize,
     ) -> TrybuildHost {
         let setup_command = match &self.host_type {
             InitializedHostType::Gcp { .. } => DEBIAN_PERF_SETUP_COMMAND,
@@ -137,11 +154,7 @@ impl ReusableHosts {
             InitializedHostType::Localhost => "", // Isn't run on localhost anyway
         };
         let host = TrybuildHost::new(self.lazy_create_host(deployment, display_name.clone()))
-            .additional_hydro_features(vec!["runtime_measure".to_string()])
-            .build_env(
-                "HYDRO_RUNTIME_MEASURE_CPU_PREFIX",
-                super::deploy_and_analyze::CPU_USAGE_PREFIX,
-            )
+            .pin_to_core(pin_to_core)
             .rustflags(self.get_rust_flags())
             .tracing(
                 TracingOptions::builder()
@@ -159,9 +172,10 @@ impl ReusableHosts {
         deployment: &mut Deployment,
         cluster_name: String,
         num_hosts: usize,
+        pin_to_core: usize,
     ) -> Vec<TrybuildHost> {
         (0..num_hosts)
-            .map(|i| self.get_no_perf_process_hosts(deployment, format!("{}{}", cluster_name, i)))
+            .map(|i| self.get_no_perf_process_hosts(deployment, format!("{}{}", cluster_name, i), pin_to_core))
             .collect()
     }
 }
