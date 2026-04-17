5095cef1f838bb1b2ab163d14133bfc48b43cf7b
David Chu <chudc@amazon.com>
2026-04-16 18:50:21 -0700

    stageleft workaround so dependencies on hydro-optimize work for now

--- hydro_optimize/src/lib.rs
+++ hydro_optimize/src/lib.rs
@@ -1,39 +1,39 @@
 #[cfg(stageleft_runtime)]
-hydro_lang::setup!();
-// #[allow(
-//     ambiguous_glob_reexports,
-//     mismatched_lifetime_syntaxes,
-//     unexpected_cfgs,
-//     unfulfilled_lint_expectations,
-//     unused,
-//     clippy::suspicious_else_formatting,
-//     clippy::type_complexity,
-//     reason = "generated code"
-// )]
-// pub mod __staged {
-//     #[cfg(any(feature = "stageleft_macro_entrypoint", stageleft_trybuild))]
-//     include!(concat!(
-//         env!("OUT_DIR"),
-//         stageleft::PATH_SEPARATOR!(),
-//         "lib_pub.rs"
-//     ));
-//
-//     #[cfg(test)]
-//     include!(concat!(
-//         env!("OUT_DIR"),
-//         stageleft::PATH_SEPARATOR!(),
-//         "staged_deps.rs"
-//     ));
-// }
-//
-// #[cfg(stageleft_runtime)]
-// #[cfg(test)]
-// mod test_init {
-//     #[ctor::ctor]
-//     fn init() {
-//         hydro_lang::compile::init_test();
-//     }
-// }
+// hydro_lang::setup!();
+#[allow(
+    ambiguous_glob_reexports,
+    mismatched_lifetime_syntaxes,
+    unexpected_cfgs,
+    unfulfilled_lint_expectations,
+    unused,
+    clippy::suspicious_else_formatting,
+    clippy::type_complexity,
+    reason = "generated code"
+)]
+pub mod __staged {
+    #[cfg(any(feature = "stageleft_macro_entrypoint", stageleft_trybuild))]
+    include!(concat!(
+        env!("OUT_DIR"),
+        stageleft::PATH_SEPARATOR!(),
+        "lib_pub.rs"
+    ));
+
+    #[cfg(test)]
+    include!(concat!(
+        env!("OUT_DIR"),
+        stageleft::PATH_SEPARATOR!(),
+        "staged_deps.rs"
+    ));
+}
+
+#[cfg(stageleft_runtime)]
+#[cfg(test)]
+mod test_init {
+    #[ctor::ctor]
+    fn init() {
+        hydro_lang::compile::init_test();
+    }
+}
 
 pub mod debug;
 pub mod decouple_analysis;
